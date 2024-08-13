ea1b4da6916782f90d2e620641b5fc7fdd1eda82
kgw10 <aa01059183132@gmail.com>
2024-08-13 17:29:09 +0900

    0813

---
+++ movieAndgame/src/main/java/com/movieAndgame/control/GameController.java
@@ -0,0 +1,34 @@
+package com.movieAndgame.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.movieAndgame.Dto.GameMemberDto;
+
+@Controller
+@RequestMapping("/game")
+public class GameController {
+
+	// localhost/game/index 매핑
+	@GetMapping("/index")
+	public String home(Model model) {
+		
+		return "game/index";
+	}
+	
+	@GetMapping("/login")
+	public String login(Model model) {
+		
+		model.addAttribute("gameMember" , new GameMemberDto());		
+		return "game/member/login";
+	}
+	
+	// 회원 가입 양식 페이지 요청
+	@GetMapping("/signUp")
+	public String signup(Model model) {
+		
+		return "game/member/join";
+	}
+}
